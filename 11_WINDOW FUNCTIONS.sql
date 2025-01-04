e41f764ab4fe69b89afc99530d31f3608a6ca114
Karol Hołda <karolholda@interia.eu>
2025-01-04 13:44:01 +0100

    Delete 11_WINDOW FUNCTIONS.sql

--- 11_WINDOW FUNCTIONS.sql
+++
@@ -1,7 +0,0 @@
-select 
-	cost_forecast_id
-	,cost_forecast
-	,row_number() over (order by cost_forecast desc)		as row_number_ranking
-	,rank() over (order by cost_forecast desc)				as rank_ranking
-	,dense_rank() over (order by cost_forecast desc)		as dense_rank_ranking
-from cost_forecast cf 
