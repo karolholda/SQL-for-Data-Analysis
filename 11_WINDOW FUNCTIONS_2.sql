381c2e770b9eb59f38a39c944c83e11a110e388f
Karol Hołda <karolholda@interia.eu>
2025-01-04 13:44:08 +0100

    Delete 11_WINDOW FUNCTIONS_2.sql

--- 11_WINDOW FUNCTIONS_2.sql
+++
@@ -1,8 +0,0 @@
-select 
-	order_id
-	,order_date
-	,delivery_city
-	,delivery_state
-	,count(order_id) over (partition by delivery_city)		as city_orders
-	,min(order_date) over (partition by delivery_state)		as first_state_order
-from orders o 
