456fa7cc7e73a7b8125ee65f98fed884311ad981
Karol Hołda <karolholda@interia.eu>
2025-01-04 13:46:03 +0100

    Delete 5_SUM AVG MIN MAX.sql

--- 5_SUM AVG MIN MAX.sql
+++
@@ -1,8 +0,0 @@
-select 
-	group_id 
-	,round(sum(product_price), 2)  as total_price
-	,round(avg(product_price), 2)  as avg_price
-	,round(min(product_price), 2)  as min_price
-	,round(max(product_price), 2)  as max_price
-from products p
-group by 1
